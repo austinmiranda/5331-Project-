5b8e2162876b034d457fb2275d731c8714fea49c
Henrique Tensfeld <tensfeld@Henriques-MacBook-Pro.local>
2018-11-09 16:27:48 -0500

    Correct treatment when lists are empty

--- SocialMarketplace/Models/Utils/ErrorHandling.cs
+++ SocialMarketplace/Models/Utils/ErrorHandling.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,7 +11,24 @@ namespace SocialMarketplace.Models.Utils
     {
         public static void AddModelError(ModelStateDictionary model, Exception ex)
         {
-            model.AddModelError(string.Empty, ex.Message);
+            if(ex is DbEntityValidationException)
+            {
+                String msg = string.Empty;
+                var e = ex as DbEntityValidationException;
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    msg += $"Entity of type \"{eve.Entry.Entity.GetType().Name}\" in state \"{eve.Entry.State}\" has the following validation errors: ";
+
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        msg += $"- Property: \"{ve.PropertyName}\", Error: \"{ve.ErrorMessage}\"";
+                    }
+
+                    model.AddModelError(string.Empty, ex.Message + " " + msg);
+                }
+            }
+            else
+                model.AddModelError(string.Empty, ex.Message + " " + ex.StackTrace.ToString());
         }
     }
 }
