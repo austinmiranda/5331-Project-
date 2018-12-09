61f284a700b4689ebc21b5977b7200c7cd5523e3
Austin Miranda <austin.miranda@outlook.com>
2018-12-09 02:52:38 -0500

    request list paging and request details

---
+++ SocialMarketplace/Models/ViewModels/Request/RequestDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using SocialMarketplace.Models.ViewModels.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialMarketplace.Models.ViewModels.Request
+{
+    public class RequestDetailsViewModel
+    {
+        public DetailViewModel Request { get; set; }
+        public IList<ResponseViewModel> Responses { get; set; }
+
+    }
+}
